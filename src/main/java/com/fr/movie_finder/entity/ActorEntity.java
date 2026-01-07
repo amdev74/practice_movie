5599c648635c8498104a2837f841807ea9e9658e
Axel <axel@exemple.com>
2026-01-07 20:07:03 +0100

    Update ActorEntity

--- src/main/java/com/fr/movie_finder/entity/ActorEntity.java
+++ src/main/java/com/fr/movie_finder/entity/ActorEntity.java
@@ -3,33 +3,48 @@ package com.fr.movie_finder.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Table(name = "actors")
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
-@Getter
-@Setter
-@ToString
+@Entity @Table(name = "actors")
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
 public class ActorEntity {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String firstname;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String lastname;
 
     @OneToMany(mappedBy = "actor", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ActorMovieEntity> movies;
+    private List<ActorMovieEntity> movies = new ArrayList<>();
 
-    public ActorEntity(String firstName, String lastName, List<ActorMovieEntity> movies) {
-        this.firstname = firstName;
-        this.lastname = lastName;
-        this.movies = movies;
+    public ActorEntity(String firstName, String lastName, List<ActorMovieEntity> movies) { this.firstname = firstName; this.lastname = lastName; this.movies = movies; }
+
+    public void addMovie(ActorMovieEntity actorMovie) {
+        movies.add(actorMovie);
+        actorMovie.setActor(this);
+    }
+
+    public void removeMovie(ActorMovieEntity actorMovie) {
+        movies.remove(actorMovie);
+        actorMovie.setActor(null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ActorEntity other)) return false;
+        return id != null && id.equals(other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
